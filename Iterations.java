31bbc21db090c6e397b78e33376bd10c7987a341
KONRAD SLOTA - STUDENT <G00354637@gmit.ie>
2018-02-21 13:52:29 +0000

    Lab Wednesday Feb 21

---
+++ Iterations.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Iterations{
+	public static void main(String[] args){
+		int i = 1;
+		int j = 2;
+		int k = 3;
+		int m = 2;
+
+	System.out.println(i == 1);
+	System.out.println(j == 3);
+	System.out.println((i >= 1) && (j < 4));
+	System.out.println((m <= 99) & (k < m));
+	System.out.println((j >= i) || (k == m));
+	System.out.println((k + m < j) | (3 - j >= k));
+	System.out.println(!(k > m));
+
+	for(i = 1; i <= 5; i++){
+		for(j = 1; j <= 3; j++){
+			for (k = 1; k <= 4; k++)
+				System.out.print('*');
+
+			System.out.println();
+		}
+		System.out.println();
+	}
+	}
+}
